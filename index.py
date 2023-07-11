e9b857612f7dc49231583e69a8f1dc472d1dbd88
Nick Cruz <nickcruz920@gmail.com>
2023-07-11 15:44:45 -0400

    Python File Handling (Opening a file)

--- index.py
+++ index.py
@@ -1,2 +1,9 @@
 # This will print the message below
 print("Hi, This is a small Python Script")
+
+# Python File Handling 
+
+# Opening a file 
+pyth = open("findme.txt")
+# alternative way to write the above code:
+pyth - open("findme.txt", "rt")
